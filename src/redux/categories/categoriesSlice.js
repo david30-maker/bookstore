96c88294d1c5971cc737cc1b115f6c32fb87d8ba
david30-maker <95672514+david30-maker@users.noreply.github.com>
2023-04-12 23:09:58 +0100

    create functionality with redux

---
+++ src/redux/categories/categoriesSlice.js
@@ -0,0 +1,20 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    categories: [],
+},
+
+const categoriesSlice = createSlice({
+    name: "categories",
+    initialState,
+    reducers: {
+        addCategory: (state) => {
+            let curState = state;
+            curState -= 'Under Construction';
+            return curState;
+        },
+    });
+
+console.log(categoriesSlice);
+
+export default categoriesSlice.reducer;
