f51043c154da488db872120b60950b39c2195792
david30-maker <95672514+david30-maker@users.noreply.github.com>
2023-04-13 18:43:42 +0100

    Add functioinalities

--- src/components/bookLists.js
+++ src/components/bookLists.js
@@ -2,11 +2,43 @@ import { useSelector } from 'react-redux';
 import BookItem from './bookItems';
 
 const BooksList = () => {
-  const books = useSelector((state) => state.books);
+  let booksArr = useSelector((store) => store.books).books;
+  const category = useSelector((store) => store.categories).categories;
+
+  const filterPer = (evet) => {
+    evet.preventDefault();
+    const categorySelected = evet.target[0].value;
+    if (category.includes(categorySelected)) {
+      if (booksArr.find((book) => book.category === categorySelected)) {
+        booksArr = booksArr.find((book) => book.category === categorySelected);
+        console.log(booksArr);
+        < BooksList />;
+      } else {
+        console.log('No books in this category');
+        booksArr = [];
+      }
+    } else {
+      console.log('No category selected');
+    }
+  };
 
   return (
     <div>
-      {books.map((book) => (
+      <h1>All Books</h1>
+      <form
+        onSubmit={(evet) => filterPer(evet)}
+        className="filter-form"
+      >
+        <select>
+          {category.map((category) => (
+            <option key={category}
+              value={category}>{category}</option>
+          ))
+          }
+        </select>
+        <button type="submit">Filter</button>
+      </form>
+      {booksArr.map((book) => (
         <div key={book.id}>
           <BookItem book={JSON.stringify(book)} />
         </div>
