644cd1a4015bc06bedfcac80366ea4c99dde2dab
david30-maker <95672514+david30-maker@users.noreply.github.com>
2023-04-15 08:14:54 +0100

    update bookitem

--- src/components/bookItems.js
+++ src/components/bookItems.js
@@ -6,13 +6,13 @@ const BookItem = ({ book, id }) => {
   const dispatch = useDispatch();
 
   return (
-    <div className="book-wrapper">
-      <div className="book-datail">
+    <div className="book-wrap">
+      <div className="book-details">
         <div className="book-category">{JSON.parse(book).category}</div>
         <div className="book-title">{JSON.parse(book).title}</div>
-        <div className="book-author">{JSON.parse(book).author}</div>
-        <div className="bool-detail-btn">
-          <button type="button" className="book-comment">Comment</button>
+        <div className="book-item-author">{JSON.parse(book).author}</div>
+        <div className="book-detail-btn">
+          <button type="button" className="book-comment">Comments</button>
           <button
             type="button"
             className="book-remove"
@@ -22,31 +22,83 @@ const BookItem = ({ book, id }) => {
               dispatch(deleteBook(evet.target.id));
             }}
           >
-            Remove Book
+            Remove
           </button>
           <button type="button" className="book-edit">Edit</button>
         </div>
       </div>
       <div className="book-status">
-        <div className="oval-2" />
+        <div className="Oval-2" />
         <div className="status-data">
           <span className="status-data-title">64%</span>
-          <span className="status-data-complete">Completed</span>
+          <span className="status-data-completed">Completed</span>
         </div>
       </div>
-      <div className="book-progress">
-        <div className="progess-title">
-          Current Chapter
-        </div>
-        <div className="progess-detail">Chapter 20</div>
+      <div className="book-item-progress">
+        <div className="progress-title">CURRENT CHAPTER</div>
+        <div className="progress-detail">Chapter 17</div>
         <button type="button" className="progress-update">Update Progress</button>
       </div>
     </div>
   );
 };
+
 BookItem.propTypes = {
   book: PropTypes.string.isRequired,
   id: PropTypes.string.isRequired,
 };
 
 export default BookItem;
+
+// import PropTypes from 'prop-types';
+// import { useDispatch } from 'react-redux';
+// import { removeBook, deleteBook } from '../redux/books/booksSlice';
+
+// const BookItem = ({ book, id }) => {
+//   const dispatch = useDispatch();
+
+//   return (
+//     <div className="book-wrapper">
+//       <div className="book-datail">
+//         <div className="book-category">{JSON.parse(book).category}</div>
+//         <div className="book-title">{JSON.parse(book).title}</div>
+//         <div className="book-author">{JSON.parse(book).author}</div>
+//         <div className="bool-detail-btn">
+//           <button type="button" className="book-comment">Comment</button>
+//           <button
+//             type="button"
+//             className="book-remove"
+//             id={id}
+//             onClick={(evet) => {
+//               dispatch(removeBook(evet.target.id));
+//               dispatch(deleteBook(evet.target.id));
+//             }}
+//           >
+//             Remove Book
+//           </button>
+//           <button type="button" className="book-edit">Edit</button>
+//         </div>
+//       </div>
+//       <div className="book-status">
+//         <div className="oval-2" />
+//         <div className="status-data">
+//           <span className="status-data-title">64%</span>
+//           <span className="status-data-complete">Completed</span>
+//         </div>
+//       </div>
+//       <div className="book-progress">
+//         <div className="progess-title">
+//           Current Chapter
+//         </div>
+//         <div className="progess-detail">Chapter 20</div>
+//         <button type="button" className="progress-update">Update Progress</button>
+//       </div>
+//     </div>
+//   );
+// };
+// BookItem.propTypes = {
+//   book: PropTypes.string.isRequired,
+//   id: PropTypes.string.isRequired,
+// };
+
+// export default BookItem;
