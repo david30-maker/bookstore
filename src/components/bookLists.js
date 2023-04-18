449c5e7fbb4882bf9d058e5084e537891e3e3d27
david30-maker <95672514+david30-maker@users.noreply.github.com>
2023-04-18 04:57:53 +0100

    add bookslice functionalities

--- src/components/bookLists.js
+++ src/components/bookLists.js
@@ -32,6 +32,33 @@ export default BooksList;
 
 // const BooksList = () => {
 //   const dispatch = useDispatch();
+//   const booksObject = useSelector((store) => store.books).books;
+
+//   useEffect(() => {
+//     dispatch(fetchBooks());
+//   }, []);
+
+//   const bookData = (Object.entries(booksObject));
+//   return (
+//     <div className="books-list-wrap">
+//       {
+//         bookData.map(
+//           (book) => <BookItem key={book[0]} id={book[0]} book={JSON.stringify(book[1][0])} />,
+//         )
+//       }
+//     </div>
+//   );
+// };
+
+// export default BooksList;
+
+// import { useDispatch, useSelector } from 'react-redux';
+// import { useEffect } from 'react';
+// import { fetchBooks } from '../redux/books/booksSlice';
+// import BookItem from './bookItems';
+
+// const BooksList = () => {
+//   const dispatch = useDispatch();
 //   const bookObject = useSelector((store) => store.books).books;
 //   useEffect(() => {
 //     dispatch(fetchBooks());
--- src/redux/books/booksSlice.js
+++ src/redux/books/booksSlice.js
@@ -1,74 +1,69 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
-// PL5veETvNbz6Vn5UAfrl
+
 const initialState = {
   books: [],
+  appID: 'GEuAWlP9fDuVTZwC5FdL',
+  url: 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/GEuAWlP9fDuVTZwC5FdL/',
 };
 
 const url = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/GEuAWlP9fDuVTZwC5FdL';
 
-export const createApps = createAsyncThunk('books/createApps',
-  async () => {
-    const resp = await axios.post('https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/');
-    return resp;
-  });
+export const createApp = createAsyncThunk('books/createApp', async () => {
+  const response = await axios.post('https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/');
+  return response;
+});
 
-export const postBooks = createAsyncThunk('books/postBooks',
-  async (newBook) => {
-    axios.post(`${url}/books`, newBook);
-  });
+export const postBooks = createAsyncThunk('books/postBooks', async (newBook) => {
+  axios.post(`${url}/books`, newBook);
+});
 
-export const fetchBooks = createAsyncThunk('books/fetchBooks',
-  async (_, thunkAPI) => {
-    try {
-      const resp = await axios.get(`${url}/books`);
-      let { books } = thunkAPI.getState().books;
-      books = resp.data;
-      return books;
-    } catch (error) {
-      return thunkAPI.rejectWithValue(error.message);
-    }
-  });
+export const fetchBooks = createAsyncThunk('books/fetchBooks', async (_, thunkAPI) => {
+  try {
+    const response = await axios.get(`${url}/books`);
+    let { books } = thunkAPI.getState().books;
+    books = response.data;
+    return books;
+  } catch (error) {
+    return thunkAPI.rejectWithValue(error.message);
+  }
+});
 
-export const deleteBook = createAsyncThunk('books/deleteBook',
-  async (bookId) => {
-    axios.delete(`${url}/books/${bookId}`);
-  });
+export const deleteBook = createAsyncThunk('books/deleteBook', async (bookId) => {
+  axios.delete(`${url}/books/${bookId}`);
+});
 
 const booksSlice = createSlice({
   name: 'books',
   initialState,
   reducers: {
-    clearBook: (state) => {
+    clearBooks: (state) => {
       const newState = state;
       newState.books = [];
       return newState;
     },
     addBook: (state, action) => {
       const newBook = action.payload;
-      const curState = state.books;
-      curState[newBook.item_id] = [{
+      const currentState = state.books;
+      currentState[newBook.item_id] = [{
         title: newBook.title,
         category: newBook.category,
         author: newBook.author,
       }];
     },
-
     removeBook: (state, action) => {
       const bookId = action.payload;
-      const curState = state;
-      // eslint-disable-next-line indent
-      delete curState.books[bookId];
-      return curState;
+      const currentState = state;
+      delete currentState.books[bookId];
+      return currentState;
     },
-    filterBook: (state, action) => {
+    filterBooks: (state, action) => {
       const category = action.payload;
-      const curState = state;
-      curState.books = state.books.find((book) => book.category === category);
-      return curState;
+      const currentState = state;
+      currentState.books = state.books.find((book) => book.category === category);
+      return currentState;
     },
   },
-
   extraReducers: {
     [fetchBooks.pending]: (state) => {
       const newState = state;
@@ -89,10 +84,109 @@ const booksSlice = createSlice({
 });
 
 export const {
-  clearBook,
+  clearBooks,
   addBook,
   removeBook,
-  filterBook,
+  filterBooks,
 } = booksSlice.actions;
 
 export default booksSlice.reducer;
+
+// import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+// import axios from 'axios';
+// // PL5veETvNbz6Vn5UAfrl
+// const initialState = {
+//   books: [],
+// };
+
+// const url = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/GEuAWlP9fDuVTZwC5FdL';
+
+// export const createApps = createAsyncThunk('books/createApps',
+//   async () => {
+//     const resp = await axios.post('https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/');
+//     return resp;
+//   });
+
+// export const postBooks = createAsyncThunk('books/postBooks',
+//   async (newBook) => {
+//     axios.post(`${url}/books`, newBook);
+//   });
+
+// export const fetchBooks = createAsyncThunk('books/fetchBooks',
+//   async (_, thunkAPI) => {
+//     try {
+//       const resp = await axios.get(`${url}/books`);
+//       let { books } = thunkAPI.getState().books;
+//       books = resp.data;
+//       return books;
+//     } catch (error) {
+//       return thunkAPI.rejectWithValue(error.message);
+//     }
+//   });
+
+// export const deleteBook = createAsyncThunk('books/deleteBook',
+//   async (bookId) => {
+//     axios.delete(`${url}/books/${bookId}`);
+//   });
+
+// const booksSlice = createSlice({
+//   name: 'books',
+//   initialState,
+//   reducers: {
+//     clearBook: (state) => {
+//       const newState = state;
+//       newState.books = [];
+//       return newState;
+//     },
+//     addBook: (state, action) => {
+//       const newBook = action.payload;
+//       const curState = state.books;
+//       curState[newBook.item_id] = [{
+//         title: newBook.title,
+//         category: newBook.category,
+//         author: newBook.author,
+//       }];
+//     },
+
+//     removeBook: (state, action) => {
+//       const bookId = action.payload;
+//       const curState = state;
+//       // eslint-disable-next-line indent
+//       delete curState.books[bookId];
+//       return curState;
+//     },
+//     filterBook: (state, action) => {
+//       const category = action.payload;
+//       const curState = state;
+//       curState.books = state.books.find((book) => book.category === category);
+//       return curState;
+//     },
+//   },
+
+//   extraReducers: {
+//     [fetchBooks.pending]: (state) => {
+//       const newState = state;
+//       newState.books = [];
+//       return newState;
+//     },
+//     [fetchBooks.fulfilled]: (state, action) => {
+//       const newState = state;
+//       newState.books = action.payload;
+//       return newState;
+//     },
+//     [fetchBooks.rejected]: (state, action) => {
+//       const newState = state;
+//       newState.books = action.payload;
+//       return newState;
+//     },
+//   },
+// });
+
+// export const {
+//   clearBook,
+//   addBook,
+//   removeBook,
+//   filterBook,
+// } = booksSlice.actions;
+
+// export default booksSlice.reducer;
